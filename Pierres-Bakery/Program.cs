a523408cf84a3306cc1f6c89380aba2dd72b9f04
gabe <gabrieltucker260@gmail.com>
2024-01-26 10:17:49 -0800

    changed naming and cleaned up code

--- Pierres-Bakery/Program.cs
+++ Pierres-Bakery/Program.cs
@@ -11,31 +11,32 @@ namespace PierreBakery
     static void Main(string[] args)
     {
       Console.WriteLine(" =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=");
-      Console.WriteLine(ShapeTracker.UserInterfaceModels.WelcomeBanner.Welcome);
+      Console.WriteLine(PierresBakery.UserInterfaceModels.WelcomeBanner.Welcome);
       Console.WriteLine(" =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=");
       Thread.Sleep(2000);
       Console.Clear();
+
       while (true)
       {
-        Console.Write(ShapeTracker.UserInterfaceModels.BreadBanner.Bread + "\n                                                                                       Bread: Buy 2, get 1 free. A single loaf costs $5. \n Enter the number of loaves of Bread you'd like: ");
+        Console.Write(PierresBakery.UserInterfaceModels.BreadBanner.Bread +
+                      "\n                                                                                       Bread: Buy 2, get 1 free. A single loaf costs $5. \n Enter the number of loaves of Bread you'd like: ");
         int breadQuantity = int.Parse(Console.ReadLine());
         Thread.Sleep(500);
         Console.Clear();
 
-
-        Console.Write(ShapeTracker.UserInterfaceModels.PastryBanner.Pastry + "\n                                                                                           Pastry: Buy 3, get 1 free. A single pastry costs $2.\nEnter the number of Pastries you'd like: ");
+        Console.Write(PierresBakery.UserInterfaceModels.PastryBanner.Pastry +
+                      "\n                                                                                           Pastry: Buy 3, get 1 free. A single pastry costs $2.\nEnter the number of Pastries you'd like: ");
         int pastryQuantity = int.Parse(Console.ReadLine());
         Thread.Sleep(500);
         Console.Clear();
 
-
         totalBreadQuantity += breadQuantity;
         totalPastryQuantity += pastryQuantity;
 
-        Bread bread = new Bread();
-        Pastry pastry = new Pastry();
+        Bread bread = new Bread(breadQuantity);
+        Pastry pastry = new Pastry(pastryQuantity);
 
-        int totalCost = bread.CalculateCost(totalBreadQuantity) + pastry.CalculateCost(totalPastryQuantity);
+        int totalCost = bread.CalculateCost() + pastry.CalculateCost();
         Console.WriteLine(" =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=");
         Console.WriteLine($"                                                                                                             You have ordered:");
         Console.WriteLine($"                                                                                                             - {totalBreadQuantity} loaves of Bread");
@@ -56,20 +57,20 @@ namespace PierreBakery
     static void DisplayReceipt()
     {
       Console.WriteLine(" =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=");
-      Console.WriteLine(ShapeTracker.UserInterfaceModels.ReceiptBanner.Receipt);
+      Console.WriteLine(PierresBakery.UserInterfaceModels.ReceiptBanner.Receipt);
       Console.WriteLine($"                                                                                                         - {totalBreadQuantity} loaves of Bread");
       Console.WriteLine($"                                                                                                         - {totalPastryQuantity} Pastries");
 
-      Bread bread = new Bread();
-      Pastry pastry = new Pastry();
+      Bread bread = new Bread(totalBreadQuantity);
+      Pastry pastry = new Pastry(totalPastryQuantity);
 
-      int totalCost = bread.CalculateCost(totalBreadQuantity) + pastry.CalculateCost(totalPastryQuantity);
+      int totalCost = bread.CalculateCost() + pastry.CalculateCost();
       Console.WriteLine($"                                                                                                         Total cost: ${totalCost} dollars");
       Console.WriteLine(" =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=");
       Thread.Sleep(2500);
       Console.Clear();
       Console.WriteLine(" =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=");
-      Console.WriteLine(ShapeTracker.UserInterfaceModels.GoodbyeBanner.Goodbye);
+      Console.WriteLine(PierresBakery.UserInterfaceModels.GoodbyeBanner.Goodbye);
       Console.WriteLine(" =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=");
     }
   }
