970b5919b56b3b939ed2501890cec1437802defb
gabe <gabrieltucker260@gmail.com>
2024-01-26 10:16:27 -0800

    refactored tests

--- Pierres-Bakery.Tests/ModelTests/BreadTests.cs
+++ Pierres-Bakery.Tests/ModelTests/BreadTests.cs
@@ -9,31 +9,22 @@ namespace PierreBakery.Tests
         [TestMethod]
         public void CalculateCost_ReturnsCorrectTotalCost_ForMultipleOfThree()
         {
-            Bread bread = new Bread();
-
-            // a multiple of 3
-            int totalCost1 = bread.CalculateCost(6);
-            Assert.AreEqual(20, totalCost1);
+            Bread bread = new Bread(6);
+            Assert.AreEqual(20, bread.CalculateCost());
         }
 
         [TestMethod]
         public void CalculateCost_ReturnsCorrectTotalCost_ForNotMultipleOfThree()
         {
-            Bread bread = new Bread();
-
-            // not a multiple of 3
-            int totalCost2 = bread.CalculateCost(5);
-            Assert.AreEqual(20, totalCost2);
+            Bread bread = new Bread(5);
+            Assert.AreEqual(20, bread.CalculateCost());
         }
 
         [TestMethod]
         public void CalculateCost_ReturnsCorrectTotalCost_ForZeroQuantity()
         {
-            Bread bread = new Bread();
-
-            // 0 quantity
-            int totalCost3 = bread.CalculateCost(0);
-            Assert.AreEqual(0, totalCost3);
+            Bread bread = new Bread(0);
+            Assert.AreEqual(0, bread.CalculateCost());
         }
     }
 }
